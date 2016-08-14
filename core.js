768b58a51a083f6e5aaa1a795a915c127cc75411
Sahil Dhanju <sahildhanju1@gmail.com>
2016-08-13 18:32:16 -0700

    got button and answerID

--- core.js
+++ core.js
@@ -1,28 +1,53 @@
 // Unique ID for the className.
-var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
+var MOUSE_VISITED_CLASSNAME = 'quicktry-visited';
 
 // Previous dom, that we want to track, so we can remove the previous styling.
 var prevDOM = null;
+var questionID = null;
+var answerID = null;
 
 // Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function (e) {
-  var srcElement = e.srcElement;
+  var srcElement = e.target;
+
+  var button = document.createElement('div');
+  button.className = 'quicktry-edit-button';
+  button.addEventListener('click', changeToEditor, false);
+  //var imgURL = chrome.extension.getURL("edit.png");
+  //console.log(imgURL);
+  //button.style.setProperty("background-image", "url(" + imgURL +")", "important");
 
   // Lets check if our underlying element is a DIV.
-  if (srcElement.nodeName == 'PRE') {
-  	console.log("Woo");
+  if (srcElement.nodeName === 'PRE') {
 
     // For NPE checking, we check safely. We need to remove the class name
     // Since we will be styling the new one after.
-    if (prevDOM != null) {
+    if (prevDOM !== null) {
       prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
+      prevDOM.removeChild(prevDOM.childNodes[0]);
     }
 
     // Add a visited class name to the element. So we can style it.
     srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
+    srcElement.style.position = "relative";
+    srcElement.style.overflow = "visible";
+    if(srcElement.firstChild.className !== "quicktry-edit-button") {
+    	srcElement.insertBefore(button, srcElement.firstChild);
+    }
+    var answerParent = findAncestor(srcElement, "answer");
+    answerID = answerParent.dataset.answerid;
 
     // The current element is now the previous. So we can remove the class
     // during the next iteration.
     prevDOM = srcElement;
   }
 }, false);
+
+function findAncestor (el, cls) {
+    while ((el = el.parentElement) && !el.classList.contains(cls));
+    return el;
+}
+
+function changeToEditor() {
+	console.log(answerID);
+}
