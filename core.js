68455fea195877e7490d056245225c488f35725a
Tim Hertweck <timhertweck@gmail.com>
2016-08-14 09:58:20 -0700

    Move the functions from utils to core.

--- core.js
+++ core.js
@@ -1,3 +1,5 @@
+var API_ENDPOINT = "http://f98e2329.ngrok.io/run";
+
 ace.require("ace/ext/language_tools");
 
 var PythonMode = ace.require("ace/mode/python").Mode;
@@ -85,6 +87,39 @@ function changeToEditor(element) {
       });
     });
   }
+}
+
+function reconstructSnippet(codeElement) {
+  var code = '';
+  var parts = codeElement.children;
+  for (var i = 0; i < parts.length; i++) {
+    code += parts[i].innerHTML;
+  }
+  return $('<div/>').html(code).text();
+}
+
+function createEditor(element) {
+  var div = document.createElement("div");
+  div.className = 'quicktry-editor';
+  var parent = element.parentNode;
+  parent.replaceChild(div, element);
+  var editor = ace.edit(div);
+  return editor;
+}
+
+function runCode(code, language, parameters, success, failure) {
+  $.ajax({
+    type: "POST",
+    url: API_ENDPOINT,
+    contentType : 'application/json',
+    data: JSON.stringify({
+      code: code,
+      lang: language,
+      params: parameters
+    }),
+    success: success,
+    error: failure
+  });
 }
 
 var Output = function($element) {
