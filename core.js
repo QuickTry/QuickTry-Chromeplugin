762c7fa000d00ebc4d1f12e46b9ff89995b600d1
Tim Hertweck <timhertweck@gmail.com>
2016-08-14 04:25:25 -0700

    Use jQuery to add the Edit button to the snippets.

--- core.js
+++ core.js
@@ -1,140 +1,110 @@
-// Unique ID for the className.
-var MOUSE_VISITED_CLASSNAME = 'quicktry-visited';
-
-// Previous dom, that we want to track, so we can remove the previous styling.
-var prevDOM = null;
-var questionID = null;
-var answerID = null;
-var codeSnipetIndex = 0;
-
-// Mouse listener for any move event on the current document.
-document.addEventListener('mousemove', function (e) {
-  var srcElement = e.target;
-
-  var button = createEditButton(changeToEditor);
-
-  // Lets check if our underlying element is a DIV.
-  if (srcElement.nodeName === 'PRE') {
-
-    // For NPE checking, we check safely. We need to remove the class name
-    // Since we will be styling the new one after.
-    if (prevDOM !== null) {
-      prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
-      prevDOM.removeChild(prevDOM.childNodes[0]);
-    }
-
-    // Add a visited class name to the element. So we can style it.
-    srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-    srcElement.style.position = "relative";
-    srcElement.style.overflow = "visible";
-    if(srcElement.firstChild.className !== "quicktry-edit-button") {
-    	srcElement.insertBefore(button, srcElement.firstChild);
-    }
-
-    // The current element is now the previous. So we can remove the class
-    // during the next iteration.
-    prevDOM = srcElement;
-  }
-}, false);
-
-function changeToEditor() {
-  var code = reconstructSnippet(prevDOM.getElementsByTagName('code')[0]);
-
-  var quickTryWrapper = document.createElement('div');
-  quickTryWrapper.className = 'quicktry-wrapper';
-
-  var editorDiv = document.createElement('div');
-  editorDiv.className = 'quicktry-editor';
-  quickTryWrapper.appendChild(editorDiv);
-
-  var toolbarDiv = document.createElement('div');
-  toolbarDiv.className = 'quicktry-toolbar';
-
-  var runButton = document.createElement('button');
-  runButton.className = 'quicktry-run';
-  runButton.innerHTML = 'Run';
-  toolbarDiv.appendChild(runButton);
-
-  var languageSelector = document.createElement('select');
-  toolbarDiv.appendChild(languageSelector);
-
-  var langPython = document.createElement("option");
-  langPython.text = 'Python2';
-  languageSelector.add(langPython);
-
-  var langPython = document.createElement("option");
-  langPython.text = 'Python3';
-  languageSelector.add(langPython);
-
-  var langJavascript = document.createElement("option");
-  langJavascript.text = 'Javascript';
-  languageSelector.add(langJavascript);
-
-  var langGo = document.createElement("option");
-  langGo.text = 'Go';
-  languageSelector.add(langGo);
-
-  quickTryWrapper.appendChild(toolbarDiv);
-
-  var outputDiv = document.createElement('div');
-  outputDiv.className = 'quicktry-output';
-  quickTryWrapper.appendChild(outputDiv);
-
-  var parent = prevDOM.parentNode;
-  parent.replaceChild(quickTryWrapper, prevDOM);
-
-  var aceEditor = ace.edit(editorDiv);
-  aceEditor.setValue(code, 0);
-
-  ace.require("ace/ext/language_tools");
-	aceEditor.setOptions({
-    enableBasicAutocompletion: true
-	});
-
-  aceEditor.setTheme("ace/theme/github");
-  var PythonMode = ace.require("ace/mode/python").Mode;
-  aceEditor.session.setMode(new PythonMode());
-
-  languageSelector.addEventListener("change", function() {
-  	var selectedLanguage = languageSelector.value;
-  	if(selectedLanguage === 'Python2' || selectedLanguage === 'Python3') {
-  		var PythonMode = ace.require("ace/mode/python").Mode;
-  		aceEditor.session.setMode(new PythonMode());
-  	} else if(selectedLanguage === 'Javascript') {
-  		var JavascriptMode = ace.require("ace/mode/javascript").Mode;
-  		aceEditor.session.setMode(new JavascriptMode());
-  	} else if(selectedLanguage === 'Go') {
-  		var GoMode = ace.require("ace/mode/golang").Mode;
-  		aceEditor.session.setMode(new GoMode());
-  	}
+$(document).ready(function() {
+  $('pre').mouseenter(function() {
+    var button = createEditButton(changeToEditor(this));
+    $(this).css('position', 'relative').prepend(button);
+  }).mouseleave(function() {
+    $('.quicktry-edit-button', this).css('position', 'static').remove();
   });
+});
 
-  var output = new Output($(outputDiv));
+function changeToEditor(element) {
+  return function() {
+    var code = reconstructSnippet(element.getElementsByTagName('code')[0]);
 
-  runButton.addEventListener('click', function() {
-    output.hide();
-    runCode(aceEditor.getValue(), languageSelector.value, '', function(response) {
-      text = response.output.replace(/(?:(\\r\\n)|(\\r)|(\\n))/g, '<br />');
-      if(response.status === 0) {
-        output.info(text);
-      } else if (response.status === 1) {
-        output.warn(text);
-      } else {
-        output.error(text);
+    var quickTryWrapper = document.createElement('div');
+    quickTryWrapper.className = 'quicktry-wrapper';
+
+    var editorDiv = document.createElement('div');
+    editorDiv.className = 'quicktry-editor';
+    quickTryWrapper.appendChild(editorDiv);
+
+    var toolbarDiv = document.createElement('div');
+    toolbarDiv.className = 'quicktry-toolbar';
+
+    var runButton = document.createElement('button');
+    runButton.className = 'quicktry-run';
+    runButton.innerHTML = 'Run';
+    toolbarDiv.appendChild(runButton);
+
+    var languageSelector = document.createElement('select');
+    toolbarDiv.appendChild(languageSelector);
+
+    var langPython = document.createElement("option");
+    langPython.text = 'Python2';
+    languageSelector.add(langPython);
+
+    var langPython = document.createElement("option");
+    langPython.text = 'Python3';
+    languageSelector.add(langPython);
+
+    var langJavascript = document.createElement("option");
+    langJavascript.text = 'Javascript';
+    languageSelector.add(langJavascript);
+
+    var langGo = document.createElement("option");
+    langGo.text = 'Go';
+    languageSelector.add(langGo);
+
+    quickTryWrapper.appendChild(toolbarDiv);
+
+    var outputDiv = document.createElement('div');
+    outputDiv.className = 'quicktry-output';
+    quickTryWrapper.appendChild(outputDiv);
+
+    var parent = element.parentNode;
+    parent.replaceChild(quickTryWrapper, element);
+
+    var aceEditor = ace.edit(editorDiv);
+    aceEditor.setValue(code, 0);
+
+    ace.require("ace/ext/language_tools");
+    aceEditor.setOptions({
+      enableBasicAutocompletion: true
+    });
+
+    aceEditor.setTheme("ace/theme/github");
+    var PythonMode = ace.require("ace/mode/python").Mode;
+    aceEditor.session.setMode(new PythonMode());
+
+    languageSelector.addEventListener("change", function() {
+      var selectedLanguage = languageSelector.value;
+      if(selectedLanguage === 'Python2' || selectedLanguage === 'Python3') {
+        var PythonMode = ace.require("ace/mode/python").Mode;
+        aceEditor.session.setMode(new PythonMode());
+      } else if(selectedLanguage === 'Javascript') {
+        var JavascriptMode = ace.require("ace/mode/javascript").Mode;
+        aceEditor.session.setMode(new JavascriptMode());
+      } else if(selectedLanguage === 'Go') {
+        var GoMode = ace.require("ace/mode/golang").Mode;
+        aceEditor.session.setMode(new GoMode());
       }
-    }, function() {
-      output.error('Something went wrong. That\'s all we know.');
-    })
-  });
+    });
+
+    var output = new Output($(outputDiv));
+
+    runButton.addEventListener('click', function() {
+      output.hide();
+      runCode(aceEditor.getValue(), languageSelector.value, '', function(response) {
+        text = response.output.replace(/(?:(\\r\\n)|(\\r)|(\\n))/g, '<br />');
+        if(response.status === 0) {
+          output.info(text);
+        } else if (response.status === 1) {
+          output.warn(text);
+        } else {
+          output.error(text);
+        }
+      }, function() {
+        output.error('Something went wrong. That\'s all we know.');
+      })
+    });
+  }
 }
 
 function createEditButton(clickHandler) {
-  var editButton = document.createElement('button');
-  editButton.className = 'quicktry-edit-button';
-  editButton.addEventListener('click', clickHandler, false);
-  var imgURL = chrome.extension.getURL("edit.png");
-  editButton.setAttribute("style", "background-image: url(" + imgURL +")");
-  return editButton;
+  var button = $('<button class="quicktry-edit-button">');
+  button.css('background-image', 'url(' + chrome.extension.getURL("edit.png") +')');
+  button.click(clickHandler);
+  return button;
 }
 
 var Output = function($element) {
